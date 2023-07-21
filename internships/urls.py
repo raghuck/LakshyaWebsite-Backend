459df25cde12456b06bc621cc8497946fc9c22fd
Ghanashyam Bhat <ghanashyambhat6@gmail.com>
2023-07-21 11:25:47 +0530

    changed the urls

--- internships/urls.py
+++ internships/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from internships.views import internship_list_view, internship_detail_view, add_internship_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('internships/', internship_list_view, name='internship_list'),
-    path('internships/detail/', internship_detail_view, name='internship_detail'),
-    path('internships/add/', add_internship_view, name='add_internship'),
+    path('list/', internship_list_view),
+    path('details/', internship_detail_view),
+    path('add/', add_internship_view)
 ]
--- internships/views.py
+++ internships/views.py
@@ -1,25 +1,23 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 from .models import Internship
 from .serializers import InternshipSerializer
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+
 def internship_list_view(request):
     internships = Internship.objects.all()
     serializer = InternshipSerializer(internships, many=True)
     return JsonResponse(serializer.data, safe=False)
 
-@api_view(['POST'])
 def internship_detail_view(request):
     internship_id = request.data.get('internship_id')
     internship = get_object_or_404(Internship, internship_id=internship_id)
     serializer = InternshipSerializer(internship)
     return JsonResponse(serializer.data)
 
-@api_view(['POST'])
-@csrf_exempt
+@login_required
 def add_internship_view(request):
     serializer = InternshipSerializer(data=request.data)
     if serializer.is_valid():
